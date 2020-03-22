b8e6d184556239895889b9ebe262e166545789fa
Andy Julow <andrew@julow.com>
2020-03-22 09:18:17 -0400

    Update places_service.dart

--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 class PlacesService {
-  final key = 'AIzaSyAoidbQgL_RDddR-h27Ypjn0kgYJEP6wWg';
+  final key = 'YOUR_KEY';
 
   Future<List<Place>> getPlaces(double lat, double lng) async {
     var response = await http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=parking&rankby=distance&key=$key');
